990609e8ecd8f73eb07b60cd7b8ecfb58ad173fc
yusufRahmatullah <ycingle@gmail.com>
2015-02-26 19:28:51 +0700

    implementasi beberapa fungsi

--- src/javastego/XinLiao.java
+++ src/javastego/XinLiao.java
@@ -5,6 +5,10 @@
  */
 package javastego;
 
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+
+
 /**
  *
  * @author Yusuf
@@ -31,22 +35,42 @@ public class XinLiao {
         return instance;
     }
     
-    public static void embedMessage(String cipher, String inputFilename, String outputFilename){
+    public static void embedMessage(String message, String key, String coverImage, String stegoImage){
         float D, D_1, D_2, D_3;
     }
     
+    public static String extractMessage(String key, String stegoImage, String coverImage){
+        String plainText="";
+        
+        return plainText;
+    }
+    
     private static float calculateD(int y0, int y1, int y2, int y3){
-        float D=1;
+        float D;
+        int yMin = min4(y0, y1, y2, y3);
+        int sumY = (y0 - yMin) + (y1 - yMin) + (y2 - yMin) + (y3 - yMin);
+        
+        D = sumY/3;
         return D;
     }
     
+    private static boolean isErrorBlock(float D, int yMin, int yMax){
+        boolean retval=false;
+        
+        return retval;
+    }
+    
+    private static char[] addBitLSB(String message, int k){
+        int size = message.length() * 8;
+        char [] retval = new char[size];
+        return retval;
+    }
+    
     private static int min4(int y0, int y1, int y2, int y3){
-        int ymin=0;
-        return ymin;
+        return min(y0, min(y1, min(y2, y3)));
     }
     
     private static int max4(int y0, int y1, int y2, int y3){
-        int ymax = 0;
-        return ymax;
+        return max(y0, max(y1, max(y1, y3)));
     }
 }
